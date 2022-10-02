69844c6697c809b6770be2093433b8cd6fb089d4
David Chiu <david20571015@gmail.com>
2022-10-02 18:19:05 +0800

    refactor: config

--- hw1/train.py
+++ hw1/train.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import os
 
 import tensorflow as tf
@@ -10,28 +11,30 @@ from src.utilis import test, train
 
 
 def main(config):
-    dataset = get_dataset(config['train_data_path'], 'train',
-                          config['batch_size'])
+    dataset = get_dataset(config['dataset']['train_path'], 'train',
+                          config['train']['batch_size'])
 
     train_size = int(dataset.cardinality().numpy() * 0.9)
     train_dataset = dataset.take(train_size)
     test_dataset = dataset.skip(train_size)
 
     model = create_model(
-        input_shape=(None, config['crop_height'], config['crop_width'], 3),
-        num_classes=config['num_classes'],
+        input_shape=(None, config['preprocess']['crop_height'],
+                     config['preprocess']['crop_width'], 3),
+        num_classes=config['model']['num_classes'],
     )
     model.summary()
 
-    optimizer = tf.keras.optimizers.Adam(learning_rate=config['learning_rate'])
+    optimizer = tf.keras.optimizers.Adam(
+        learning_rate=config['train']['learning_rate'])
 
     train_writer = tf.summary.create_file_writer(  # type: ignore
         config['train_log_dir'])
     test_writer = tf.summary.create_file_writer(  # type: ignore
         config['test_log_dir'])
 
-    for epoch in range(1, config['num_epochs'] + 1):
-        print(f'Epoch {epoch}/{config["num_epochs"]}:')
+    for epoch in range(1, config['train']['num_epochs'] + 1):
+        print(f'Epoch {epoch}/{config["train"]["num_epochs"]}:')
 
         train(model, optimizer, train_dataset, train_writer, epoch)
         test(model, test_dataset, test_writer, epoch)
@@ -44,8 +47,15 @@ if __name__ == '__main__':
         cfg = yaml.load(f, Loader=yaml.FullLoader)
 
     current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    cfg['log_path'] = os.path.join(cfg['log_path'], current_time)
-    cfg['train_log_dir'] = os.path.join(cfg['log_path'], 'tensorboard', 'train')
-    cfg['test_log_dir'] = os.path.join(cfg['log_path'], 'tensorboard', 'test')
+    cfg['log_dir'] = os.path.join(cfg['log_dir'], current_time)
+    os.makedirs(cfg['log_dir'], exist_ok=True)
+
+    with open(os.path.join(cfg['log_dir'], 'config.yaml'),
+              mode='w',
+              encoding='utf-8') as f:
+        yaml.dump(cfg, f, default_flow_style=False)
+
+    cfg['train_log_dir'] = os.path.join(cfg['log_dir'], 'tensorboard', 'train')
+    cfg['test_log_dir'] = os.path.join(cfg['log_dir'], 'tensorboard', 'test')
 
     main(cfg)
